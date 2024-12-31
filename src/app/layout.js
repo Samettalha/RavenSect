30c6ae29e550befa0814953fa36ee9c9b6855049
Samettalha <141047342+Samettalha@users.noreply.github.com>
2024-12-31 18:35:42 +0300

    arkaplan renk düzenlemsi

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,7 +11,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className="bg-[#0a0917]">
+      <body className="bg-[#06061a]">
         <Navbar />
         {children}
         <Footer />
